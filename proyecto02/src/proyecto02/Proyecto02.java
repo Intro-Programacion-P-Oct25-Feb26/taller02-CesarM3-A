6b937e269395495783528ac85a6bb1b43aa2ad30
CesarM3-A <cesarmateoab2007@gmail.com>
2025-10-16 11:52:55 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de Java"+
+        "Versionado a traves de Git"+
+        "Manejado desde GitHub"+
+        "Ademas de usar GitClassroom");
+// TODO code application logic here
+    }
+    
+}
